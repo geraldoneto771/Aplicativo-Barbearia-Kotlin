31dae73053502f41aa594b65cf2fd12349dfbc86
geraldo.pires <geraldopireslacerda771@gmail.com>
2021-09-22 10:17:14 -0300

    "Registro de corte com inserção de imagens e validação com radioGroup"

--- app/src/main/java/com/example/barbeariakotlin/ui/registre/CadastroCorteFragment.kt
+++ app/src/main/java/com/example/barbeariakotlin/ui/registre/CadastroCorteFragment.kt
@@ -1,7 +1,10 @@
 package com.example.barbeariakotlin.ui.registre
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -16,6 +19,7 @@ import androidx.core.content.FileProvider
 import androidx.navigation.fragment.findNavController
 import com.example.barbeariakotlin.databinding.FragmentCadastroCorteBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.nav_header_main.*
 import java.io.File
 import java.io.IOException
 import kotlin.jvm.Throws
@@ -34,104 +38,45 @@ class CadastroCorteFragment : Fragment() {
 
         mBinding = FragmentCadastroCorteBinding.inflate(inflater)
 
+        mBinding.comprovativo.setOnClickListener{
+
+            mBinding.addImageCorte.visibility = View.VISIBLE
+        }
+
+        mBinding.exclusivo.setOnClickListener{
+
+            mBinding.addImageCorte.visibility = View.GONE
+        }
+
         mBinding.addImageCorte.setOnClickListener {
-            if (allPermissionsGranted()) {
-                dispatchTakePictureIntent()
-            } else {
-                ActivityCompat.requestPermissions(
-                    requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-                )
-            }
+            openGalleryForImage()
+        }
+
+        mBinding.buttonSaveCorte.setOnClickListener {
+                if (mBinding.exclusivo.isChecked)
+                Snackbar.make(mBinding.root,"Salvo com sucesso", Snackbar.LENGTH_LONG).show()
+                else
+                Snackbar.make(mBinding.root,"É obrigatório anexar uma foto", Snackbar.LENGTH_LONG).show()
+
         }
 
         return mBinding.root
 
     }
 
-    val takePicture =
-        registerForActivityResult(ActivityResultContracts.TakePicture()) {
-
-            if (it) {
-                Snackbar.make(mBinding.root, "Anexado com sucesso", Snackbar.LENGTH_LONG).show()
-
-                findNavController().navigateUp()
-            } else {
-                Snackbar.make(mBinding.root, "É obrigatório anexar uma foto", Snackbar.LENGTH_LONG)
-                    .show()
-            }
-        }
-
-    var photoFile: File? = null
-    lateinit var photoURI: Uri
-
-    private fun dispatchTakePictureIntent() {
-        photoFile = try {
-            createImageFile()
-        } catch (ex: IOException) {
-            null
-        }
-        photoFile?.also {
-            photoURI = FileProvider.getUriForFile(
-                requireContext(), "", it
-            )
-            takePicture.launch(photoURI)
-        }
-    }
-
-    lateinit var currentPhotoPath: String
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val name: String = "photo"
-        val storageDir: File? = getOutputDirectory()
-        return File.createTempFile(
-            name, /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
-        }
-    }
-
-    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-    private val REQUEST_CODE_PERMISSIONS = 10
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            requireContext(), it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                dispatchTakePictureIntent()
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            }
-        }
-    }
-
-    fun getOutputDirectory(): File {
-        val mediaDir = requireActivity().externalMediaDirs.firstOrNull()?.let {
-            File(it, "project").apply { mkdirs() }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else requireActivity().filesDir
-
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE)
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+            imageView.setImageURI(data?.data) // handle chosen image
+        }
+    }
+
+    val REQUEST_CODE = 100
 }

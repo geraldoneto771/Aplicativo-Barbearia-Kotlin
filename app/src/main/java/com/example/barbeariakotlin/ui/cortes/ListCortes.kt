3082d0e47fc687d292aad6c8f68dcec4b8fdc0b4
geraldo.pires <geraldopireslacerda771@gmail.com>
2021-09-27 08:43:26 -0300

    "Layout lista de cortes"

---
+++ app/src/main/java/com/example/barbeariakotlin/ui/cortes/ListCortes.kt
@@ -0,0 +1,26 @@
+package com.example.barbeariakotlin.ui.cortes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.barbeariakotlin.databinding.FragmentListCortesBinding
+
+
+class ListCortes : Fragment() {
+
+    private lateinit var mBinding: FragmentListCortesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        mBinding = FragmentListCortesBinding.inflate(inflater)
+
+
+        return mBinding.root
+    }
+
+}

006c14e20c5b3489cfb2a8e116664d5f1e7ca7d9
geraldo.pires <geraldopireslacerda771@gmail.com>
2021-09-28 11:14:42 -0300

    "Criação do layout de descrição de corte!!!"

---
+++ app/src/main/java/com/example/barbeariakotlin/descriptionCorte.kt
@@ -0,0 +1,30 @@
+package com.example.barbeariakotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.barbeariakotlin.databinding.FragmentDescriptionCorteBinding
+import com.example.barbeariakotlin.ui.cortes.CorteViewModel
+
+
+class descriptionCorte : Fragment() {
+
+    private lateinit var mBinding: FragmentDescriptionCorteBinding
+    private lateinit var mViewModel: CorteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mBinding = FragmentDescriptionCorteBinding.inflate(inflater)
+        mViewModel = ViewModelProvider(requireActivity()).get(CorteViewModel::class.java)
+
+        // Inflate the layout for this fragment
+        return mBinding.root
+    }
+
+}
